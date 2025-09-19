d9f8bae6bd28e19555e85108a55bb17a866fb12d
pongb12 <st163943@gmail.com>
2025-09-19 19:06:04 +0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [79393329652220] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", --Defusal FPS --
-  [5670218884] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", --item asylum--
+  [79393329652220] = "https://raw.githubusercontent.com/pongb12/SaygexScript/refs/heads/main/PongbScript.lua", --Defusal FPS --
+  [5670218884] = "https://raw.githubusercontent.com/pongb12/SaygexScript/refs/heads/main/Items.lua", --item asylum--
 }
 return Games
